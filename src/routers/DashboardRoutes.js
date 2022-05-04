4f9ba20016f897d90989845d598b1b6b0c0d962e
Javier Cruz <javier.cruz.bonoso@avanade.com>
2022-05-04 19:54:17 +0200

    segundo router

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { DcScreen } from "../components/dc/DcScreen"
+import { MarvelScreen } from "../components/marvel/MarvelScreen"
+import { HeroScreen } from "../components/hero/HeroScreen"
+import { SearchScreen } from "../components/search/SearchScreen"
+import { Navbar } from "../components/ui/NavBar"
+
+export const DashboardRoutes = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/marvel" element={<MarvelScreen />} />
+        <Route path="/dc" element={<DcScreen />} />
+        <Route path="/hero" element={<HeroScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/" element={<MarvelScreen />} />
+      </Routes>
+    </div>
+  )
+}
