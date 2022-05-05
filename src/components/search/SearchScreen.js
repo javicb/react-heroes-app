42ddcc6ad984caf132cdc6dfbd31fa651829b19a
Javier Cruz <javier.cruz.bonoso@avanade.com>
2022-05-05 20:09:00 +0200

    mostrar listado de héroes

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,5 +1,6 @@
-import React from 'react'
 import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
 
@@ -8,6 +9,7 @@ export const SearchScreen = () => {
   });
 
   const { searchText } = formValues;
+  const heroesFiltered = getHeroesByName(searchText);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -28,6 +30,16 @@ export const SearchScreen = () => {
             <input type='text' className='form-control' placeholder='Buscar un héroe' name='searchText' autoComplete='off' value={searchText} onChange={handleInputChange} />
             <button type='submit' className='btn btn-outline-primary mt-1'>Buscar</button>
           </form>
+        </div>
+        <div className='col-7'>
+          <h4>Resultados</h4>
+          <hr />
+          <ul className='list-group'>
+
+            {heroesFiltered.map(hero => (
+              <HeroCard key={hero.id} {...hero} />
+            ))}
+          </ul>
         </div>
       </div>
     </div>
---
+++ src/selectors/getHeroesByName.js
@@ -0,0 +1,5 @@
+import { heroes } from '../data/heroes';
+
+export const getHeroesByName = (name = '') => {
+  return heroes.filter((hero) => hero.superhero.toLowerCase().includes(name.toLowerCase()));
+}
