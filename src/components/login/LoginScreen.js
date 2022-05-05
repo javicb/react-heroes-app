0616654a7ef284accf5a6a0177da14a91accb500
Javier Cruz <javier.cruz.bonoso@avanade.com>
2022-05-05 13:44:35 +0200

    useNavigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 export const LoginScreen = () => {
+
+  const navigate = useNavigate();
+
+
+  const handleLogin = () => {
+    navigate('/', { replace: true });
+  }
+
   return (
-    <div>
-      <h1>Login Screen</h1>
+    <div className='container mt-5'>
+      <div>
+        <h1>Login</h1>
+        <hr />
+      </div>
+      <div>
+        <button className='btn btn-primary' onClick={handleLogin} >Login</button>
+      </div>
     </div>
   )
 }
